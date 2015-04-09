84857b07f7a0c77d08a571207904d650789c34a9
kiran4399 <kiran4399@gmail.com>
2015-04-09 19:12:59 +0530

    Designated authors

--- connection.c
+++ connection.c
@@ -1,8 +1,8 @@
 /***************************************************************************
-*Author:                                                                   
-*Date:                                                                     
-*Filename:                                                                 
-*Description:                                                              
+*Author: Kiran Kumar Lekkala	                                                                  
+*Date:  10 April 2015                                                                   
+*Filename: connection.c                                                                
+*Description: Initializing connections for Downloading/Uploading                                                             
                                                                            
                                                                            
 ***************************************************************************/
--- current_process.c
+++ current_process.c
@@ -1,8 +1,27 @@
 /***************************************************************************
-*Author:                                                                   
-*Date:                                                                     
-*Filename:                                                                 
-*Description:                                                              
-                                                                           
-                                                                           
+*Author:
+*Date:
+*Filename:
+*Description:
 ***************************************************************************/
+
+#include "current.h"
+
+
+
+static const char *requests={"IndexGet",
+                             "ShortList",
+                             "Longlist",
+                             "RegEx",
+                             "Verify",
+                             "Check_All",
+                             "FileDownload",
+                             "FileUpload" };
+
+
+
+
+
+int process_init(char* file, char* output){
+    FILE*
+}
