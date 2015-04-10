60d50fdba01d096fd22cf32b181598e6c043e112
kiran4399 <kiran4399@gmail.com>
2015-04-10 21:37:59 +0530

    commits

--- chunk.c
+++ chunk.c
@@ -9,14 +9,4 @@
 
 
 
-int make_chunks(FILE *fp, uint8_t **chunk_hashes);
 
-
-/* return the md-5 hash of the string */
-void md-5_hash(uint8_t *chr, int len, uint8_t *t);
-
-/* converts hex string to ascii */
-void bin2hex(uint8_t *buf, int len, char *ascii);
-
-/* converts an ascii to hex */
-void ascii2hex(char *ascii, int len, uint8_t *buf);
