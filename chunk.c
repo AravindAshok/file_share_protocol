09e96257ce7ce420d3873b8143967c22d3f77b4e
kiran4399 <kiran4399@gmail.com>
2015-04-10 21:57:09 +0530

    commits

--- chunk.c
+++ chunk.c
@@ -11,6 +11,23 @@ void md5_hash(uint8_t *str, int len, uint8_t *hash){
 
 
 
+int make_chunks(FILE *fp, uint8_t *chunk_hashes){
 
+    uint8_t *buffer = (uint8_t*) malloc(BT_CHUNK_SIZE);
+    int numchunks = 0;
+    int numbytes = 0;
+
+    /* read the bytes from the file and fill in the chunk hashes */
+    while((numbytes = fread(buffer)))
+}
 }
 
+int main(int argc, char *argv[]){
+    uint8_t *test = "test";
+    uint8_t hash1[MD5_HASH_SIZE], hash2[MD5_HASH_SIZE];
+    char ascii[MD5_HASH_SIZE*2 + 1];
+
+    md5_hash(test, 4, hash);
+
+    binary2
+}
