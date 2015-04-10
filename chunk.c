6def84bdc0680534842ea9e0349d0f65bf2b07e4
kiran4399 <kiran4399@gmail.com>
2015-04-10 22:17:52 +0530

    commits

--- chunk.c
+++ chunk.c
@@ -11,11 +11,23 @@ void md5_hash(uint8_t *str, int len, uint8_t *hash){
     struct md5_ctx ctx;
     unsigned char digest(16);
 
+    //init the md5 process
     md5_init(&ctx);
 
     ctx.size = len ? len:strlen(chaine);
     strcpy(ctx.buf, chaine);
+    md5_update(&ctx);
+    md5_final(digest, &ctx);
+    hash = digest;
+}
 
+void bin2hex(uint8_t *buf, int len, uint8_t *hex){
+    int i;
+    for(i=0;i<len;i++){
+        sprintf(hex+(i*2), "%.2x", buf[i]);
+    }
+    hex[len*2] = 0;
+}
 
 
 int make_chunks(FILE *fp, uint8_t *chunk_hashes){
