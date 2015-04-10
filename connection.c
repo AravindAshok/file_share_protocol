66d614b913bd00c964937f995f9c99a928e6257c
kiran4399 <kiran4399@gmail.com>
2015-04-10 16:39:43 +0530

    commits

--- connection.c
+++ connection.c
@@ -1,8 +1,8 @@
 /***************************************************************************
-*Author: Kiran Kumar Lekkala	                                                                  
-*Date:  10 April 2015                                                                   
-*Filename: connection.c                                                                
-*Description: Initializing connections for Downloading/Uploading                                                             
-                                                                           
-                                                                           
+*Author: Aravind Ashok
+*Date:  10 April 2015
+*Filename: connection.c
+*Description: Initializing connections for Downloading/Uploading
+
+
 ***************************************************************************/
