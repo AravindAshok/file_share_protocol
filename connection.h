3cf4597b95f8035c3a1e25265f28db01f833ee8e
kiran4399 <kiran4399@gmail.com>
2015-04-21 22:30:06 +0530

    modifications

--- connection.h
+++ connection.h
@@ -1,6 +1,5 @@
-
-#ifndef _CONN_H
-#define _CONN_H
+#ifndef _CONNECTION_H
+#define _CONNECTION_H
 
 #include "queue.h"
 #include "chunk.h"
@@ -12,7 +11,7 @@
 #define INIT_SSTHRESH 64  // initial thresh size
 
 typedef struct download_connection {
-	bt_peer_t* provider;
+	peer_t* provider;
 	queue_t* chunks;
 	queue_t* get_queue;
 	struct timeval last_time;
@@ -21,7 +20,7 @@ typedef struct download_connection {
 
 
 typedef struct upload_connection {
-	bt_peer_t* receiver;
+	peer_t* receiver;
 	data_packet_t** pkt_array;
 	int l_ack;
 	int l_available;
@@ -31,13 +30,13 @@ typedef struct upload_connection {
 }upload_connection_t;
 
 typedef struct download_pool {
-	down_conn_t** connection;
+	download_connection_t** connection;
 	int* flag;
 	int num;
 }download_pool_t;
 
 typedef struct upload_pool {
-	up_conn_t** connection;
+	upload_connection_t** connection;
 	int* flag;
 	int num;	
 }upload_pool_t;
@@ -46,7 +45,7 @@ typedef struct upload_pool {
 void init_download_pool(download_pool_t* pool);
 void init_upload_pool(upload_pool_t* pool);
 void init_download_connection(download_connection_t** conn, peer_t* provider, queue_t* chunk, queue_t* get_queue);
-void init_upload_connection(upload_connection_t** conn, bt_peer_t* receiver, data_packet_t** pkt_array);
+void init_upload_connection(upload_connection_t** conn, peer_t* receiver, data_packet_t** pkt_array);
 
 //Adding the respective connections to the pool
 download_connection_t* add_download_pool(download_pool_t* pool, peer_t* provider, queue_t* chunk, queue_t* get_queue);
@@ -59,8 +58,8 @@ void delete_upload_pool(upload_pool_t* pool, peer_t* peer);
 //Get the pointers to get download connection
 download_connection_t* get_download_connection(download_pool_t* pool, peer_t* peer);
 upload_connection_t* get_update_connection(upload_pool_t* pool, peer_t* peer);
-void up_conn_recur_send(up_conn_t* conn, struct sockaddr* to);
-void update_download_connection( down_conn_t* conn, bt_peer_t* peer);
+void upload_connection_recur_send(upload_connection_t* conn, struct sockaddr* to);
+void update_download_connection( down_connection_t* conn, peer_t* peer);
 void update_upload_connection(upload_connection_t* conn, peer_t* peer, data_packet_t* get_pkt);
 
 #endif
